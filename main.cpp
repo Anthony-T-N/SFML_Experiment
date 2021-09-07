0e926d69587938484c28938c3de15bbbc5976f9a
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2021-09-07 19:50:49 +0930

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
+// https://en.sfml-dev.org/forums/index.php?topic=28191.0
+/* 
+Note to self: This makes no sense at all. Solution was to change from debug to release and it works. Despite the property pages only linking to static/debug libs files.
+Further investigation is required to make sense of the files operating in the background.
+
+*/
+
 #include <SFML/Graphics.hpp>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(400, 400), "SFML works!");
     sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    shape.setFillColor(sf::Color::Red);
 
     while (window.isOpen())
     {
@@ -22,3 +29,4 @@ int main()
 
     return 0;
 }
+
