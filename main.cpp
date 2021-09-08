30a2f0934ddc5234546323d924fef09d3871f3be
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2021-09-08 21:43:21 +0930

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+
 // https://en.sfml-dev.org/forums/index.php?topic=28191.0
 /* 
 Note to self: This makes no sense at all. Solution was to change from debug to release and it works. Despite the property pages only linking to static/debug libs files.
@@ -9,9 +10,15 @@ Further investigation is required to make sense of the files operating in the ba
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(400, 400), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Red);
+    sf::RenderWindow window(sf::VideoMode(500, 500), "<====Title====>");
+    sf::CircleShape shape(50.f);
+    shape.setFillColor(sf::Color::Green);
+
+    sf::CircleShape shape_two(25.f);
+    shape_two.setFillColor(sf::Color::Red);
+
+    //sf::RectangleShape rectangle(sf::Vector2f(120.f, 50.f));
+    //rectangle.setSize(sf::Vector2f(100.f, 100.f));
 
     while (window.isOpen())
     {
@@ -22,11 +29,30 @@ int main()
                 window.close();
         }
 
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
+        {
+            shape.move(sf::Vector2f(-0.5f, 0.0));
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+        {
+            shape.move(sf::Vector2f(0.5f, 0.0));
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
+        {
+            shape.move(sf::Vector2f(0.0, -0.5f));
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
+        {
+            shape.move(sf::Vector2f(0.0, +0.5f));
+        }
+
         window.clear();
         window.draw(shape);
+        window.draw(shape_two);
+        //window.draw(rectangle);
         window.display();
+
     }
 
     return 0;
 }
-
